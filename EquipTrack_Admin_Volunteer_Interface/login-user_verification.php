dd0b5142d6cf96d29cd3daba66fbc826c8048fd3
charlsrms <aramos.a12345006@umak.edu.ph>
2025-05-29 00:48:49 +0800

    Update login-user_verification.php

--- EquipTrack_Admin_Volunteer_Interface/login-user_verification.php
+++ EquipTrack_Admin_Volunteer_Interface/login-user_verification.php
@@ -5,7 +5,7 @@ require 'db.php'; // Database connection
 // Check if the user is logged in
 if (!isset($_SESSION['user_id'])) {
     // User not logged in, redirect to login page
-    header('Location: login.php');
+    header('Location: index.php');
     exit;
 }
 
