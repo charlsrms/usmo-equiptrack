fa192d55ff4fe89bdef31920ab58eafce1fc06b9
charlsrms <aramos.a12345006@umak.edu.ph>
2025-05-29 00:46:05 +0800

    Update add-equipment.php

--- EquipTrack_Admin_Volunteer_Interface/add-equipment.php
+++ EquipTrack_Admin_Volunteer_Interface/add-equipment.php
@@ -6,7 +6,7 @@ error_log("Form submitted: " . print_r($_POST, true));
 session_start();
 
 if (!isset($_SESSION['user_id'])) {
-  header('Location: login.php');
+  header('Location: index.php');
   exit;
 }
 
