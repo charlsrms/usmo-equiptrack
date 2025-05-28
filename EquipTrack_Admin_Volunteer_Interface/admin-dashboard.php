4a7005e5675e82c5b94e02bc514e22f438a8b27e
charlsrms <aramos.a12345006@umak.edu.ph>
2025-05-29 00:44:54 +0800

    Update admin-dashboard.php

--- EquipTrack_Admin_Volunteer_Interface/admin-dashboard.php
+++ EquipTrack_Admin_Volunteer_Interface/admin-dashboard.php
@@ -4,7 +4,7 @@ session_start();
 
 // Check if user is logged in and is admin
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
-  header('Location: login.php');
+  header('Location: index.php');
   exit;
 }
 
